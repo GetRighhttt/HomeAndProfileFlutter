cc5fd871fdda206fccfb54d226f1909f7fceffa2
Stefan Bayne <stefanbusiness95@gmail.com>
2023-06-09 16:19:40 -0400

    next commit.

--- home_and_profile/lib/pages/home_page.dart
+++ home_and_profile/lib/pages/home_page.dart
@@ -8,8 +8,35 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'FlutterDemo',
+          'Home',
         ),
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+            );
+          },
+        ),
+        primary: true,
+        centerTitle: true,
+        elevation: 4,
+        shadowColor: Colors.lightBlue,
+      ),
+      body: const Column(
+        children: [
+          Card(
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Hello world',
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
