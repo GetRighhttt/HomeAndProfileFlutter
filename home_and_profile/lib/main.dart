c76f0b5ea086479e14d806290609f886769a287f
Stefan Bayne <stefanbusiness95@gmail.com>
2023-06-09 17:27:03 -0400

    more refactoring.

--- home_and_profile/lib/main.dart
+++ home_and_profile/lib/main.dart
@@ -1,23 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:home_and_profile/pages/home_page.dart';
+import 'package:home_and_profile/pages/my_app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-      ),
-      home: const HomePage(),
-    );
-  }
 }
